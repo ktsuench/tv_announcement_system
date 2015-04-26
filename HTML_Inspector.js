1ef5c0d57f24fd3193b54b433ec8050539fc9786
will289 <will289@hotmail.com>
2015-04-26 19:51:01 -0400

    Empty Repository

--- HTML_Inspector.js
+++
@@ -1,4 +0,0 @@
-var htmlInsp = document.createElement('script');
-htmlInsp.src = 'http://cdnjs.cloudflare.com/ajax/libs/html-inspector/0.8.1/html-inspector.js';
-document.body.appendChild(htmlInsp);
-//HTMLInspector.inspect();
